74cf365699ab0eced8f96d63003a7c15997cd11c
Wang, Gary (Web Development) <Gary.Wang@tdsb.on.ca>
2026-06-30 22:25:33 -0400

    Added playwright-core and @sparticuz/chromium etc

--- netlify/functions/archive-background.mjs
+++ netlify/functions/archive-background.mjs
@@ -1,12 +1,16 @@
 import * as netlifyBlobsBundle from '@netlify/blobs';
 import archiverBundle from 'archiver';
 import * as cheerioBundle from 'cheerio';
+import * as playwrightCoreBundle from 'playwright-core';
+import chromiumBundle from '@sparticuz/chromium';
 import archiveBackgroundHandler from '../lib/archive-background-handler.cjs';
 
 const { handler } = archiveBackgroundHandler;
 void netlifyBlobsBundle;
 void archiverBundle;
 void cheerioBundle;
+void playwrightCoreBundle;
+void chromiumBundle;
 
 export const config = {
   background: true
